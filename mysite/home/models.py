67b7b62adc87838daf34910c76bfa10f6a42377f
Sean Waxman <seanwaxman17@gmail.com>
2018-01-27 08:37:26 -0500

    Updated backend

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -4,22 +4,22 @@ class TA(models.Model):
 	#Properties of TA and the courses they teach
 	taID = models.CharField(max_length=20)
 	taCode = models.CharField(max_length=20)
-	taCourses = models.ForeignKey(Course)
+	#taCourses = models.ForeignKey(Course)
 	#taCourses = models.ManyToManyField(Course)
 
 class Course(models.Model):
 	#has identifying code/name and list of TA's for the course
 	courseName = models.CharField(max_length=20)
 	courseCode = models.CharField(max_length=20)
-	courseTas = models.ForeignKey(TA)
-	#courseTAs = models.ManyToManyField(TA)
+	#courseTas = models.ForeignKey(TA)
+	courseTAs = models.ManyToManyField(TA)
 
 class Student(models.Model):
 	#has identifying properties so they can be contacted
 	studentName = models.CharField(max_length=30)
 	studentID = models.CharField(max_length=20)
 	studentEmail = models.CharField(max_length=50)
-	studentPhone= models.Charfield(max_length=20)
+	studentPhone= models.CharField(max_length=20)
 
 class Queue(models.Model):
 	#has TA, course and student
