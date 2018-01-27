8ff633add3f6191828cc9f1aef8acbe05068c085
Sean Waxman <seanwaxman17@gmail.com>
2018-01-27 11:00:25 -0500

    Backend stuff still broke

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,10 +1,19 @@
 from django.db import models
-from djnago.forms import ModelForm
+from django.forms import ModelForm
+
+class Student(models.Model):
+	#has identifying properties so they can be contacted
+	studentName = models.CharField(max_length=30)
+	studentID = models.CharField(max_length=20)
+	studentEmail = models.CharField(max_length=50)
+	studentPhone= models.CharField(max_length=20)
 
 class TA(models.Model):
 	#Properties of TA and the courses they teach
 	taID = models.CharField(max_length=20)
 	taCode = models.CharField(max_length=20)
+	taName = models.CharField(max_length=30)
+	currentStudent = models.CharField(max_length=30)
 	#taCourses = models.ForeignKey(Course)
 	#taCourses = models.ManyToManyField(Course)
 
@@ -14,26 +23,20 @@ class Course(models.Model):
 	courseCode = models.CharField(max_length=20)
 	#courseTas = models.ForeignKey(TA)
 	courseTAs = models.ManyToManyField(TA)
-
-class Student(models.Model):
-	#has identifying properties so they can be contacted
-	studentName = models.CharField(max_length=30)
-	studentID = models.CharField(max_length=20)
-	studentEmail = models.CharField(max_length=50)
-	studentPhone= models.CharField(max_length=20)
+	#availableTAs = models.ManyToManyField(TA)
 
 class Queue(models.Model):
 	#has TA, course and student
-	queueSize = models.IntegerField()
-	ta = models.ManyToManyField(TA)
+	queueSize = models.NumberField(default=0)
+	#ta = models.ManyToManyField(TA)
 	students = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 #Forms for each database created (possibly irrelevant)
 
-"""class TAForm(ModelForm):
+class TAForm(ModelForm):
 	class Meta:
 		model = TA
-		fields = ['taID', 'taCode']
+		fields = ['taID', 'taCode', 'taName', 'currentStudent']
 
 class CourseForm(ModelForm):
 	class Meta:
@@ -42,11 +45,10 @@ class CourseForm(ModelForm):
 
 class StudentForm(ModelForm):
 	class Meta:
-		model = student
-		field = ['studentName', 'studentID', 'studentEmail', 'studentPhone']
+		model = Student
+		fields = ['studentName', 'studentID', 'studentEmail', 'studentPhone']
 
 class QueueForm(ModelForm):
 	class Meta:
 		model = Queue
-		fields = ['queueSize', 'ta', 'students']"""
-
+		fields = ['queueSize', 'students']
