449beac7e9e56f4b2e480de066055f5df3f2c06b
Sean Waxman <seanwaxman17@gmail.com>
2018-01-27 13:59:19 -0500

    Auto stash before merge of "master" and "origin/master"

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -5,25 +5,47 @@ class Student(models.Model):
 	#has identifying properties so they can be contacted
 	studentName = models.CharField(max_length=30)
 	studentID = models.CharField(max_length=20)
-	studentIssue = models.CharField(max_length=200, default = "")
+	studentIssue = models.CharField(max_length=200)
 	studentEmail = models.CharField(max_length=50)
 	studentPhone= models.CharField(max_length=20)
 
-class TA(models.Model):
+	def __str__(self):
+		return self.studentName
+
+class TA(models.Model): #Courses taught + need mulitple fucntionality
+
 	#Properties of TA and the courses they teach
 	taID = models.CharField(max_length=20)
 	taCode = models.CharField(max_length=20)
 	taName = models.CharField(max_length=30)
+<<<<<<< Updated upstream
 	currentStudent = models.CharField(max_length=30, default='')
 	#taCourses = models.ForeignKey(Course)
+=======
+	currentStudent = models.CharField(max_length=30, null = True, blank = True)
+	taCourses = []
+
+	#Check whether current student can be blank
+
+	def __str__(self):
+		return self.taName
+	def addTACourse(courseAdding):
+		taCourses.append(courseAdding)
+	def removeTACourse(courseDropping):
+		taCourses.remove(courseDropping)
+
+>>>>>>> Stashed changes
 	#taCourses = models.ManyToManyField(Course)
 
 class Course(models.Model):
 	#has identifying code/name and list of TA's for the course
 	courseName = models.CharField(max_length=20)
 	courseCode = models.CharField(max_length=20)
-	#courseTas = models.ForeignKey(TA)
 	courseTAs = models.ManyToManyField(TA)
+
+	def __str__(self):
+		return self.courseName
+	#courseTas = models.ForeignKey(TA)
 	#availableTAs = models.ManyToManyField(TA)
 
 class Queue(models.Model):
@@ -31,6 +53,9 @@ class Queue(models.Model):
 	queueSize = models.IntegerField(default=0, blank = True,  null = True)
 	course = models.ManyToManyField(Course)
 	students = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "Queue for: " % course
 
 #Forms for each database created (possibly irrelevant)
 
@@ -53,3 +78,5 @@ class QueueForm(ModelForm):
 	class Meta:
 		model = Queue
 		fields = ['queueSize', 'course', 'students']
+
+		
